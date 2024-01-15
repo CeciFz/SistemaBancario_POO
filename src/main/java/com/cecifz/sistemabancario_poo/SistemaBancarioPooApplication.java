6ffbc9455de8bf22c0254f1a50ce30da66620a05
Ceci <ceci.fz87@gmail.com>
2024-01-14 23:40:44 -0300

    Initial commit

---
+++ src/main/java/com/cecifz/sistemabancario_poo/SistemaBancarioPooApplication.java
@@ -0,0 +1,13 @@
+package com.cecifz.sistemabancario_poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBancarioPooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaBancarioPooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cecifz/sistemabancario_poo/SistemaBancarioPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cecifz.sistemabancario_poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioPooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
