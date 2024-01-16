f0503ab055ab83bd82bdaece49c24f80d371bc3f
Ceci <ceci.fz87@gmail.com>
2024-01-15 22:07:50 -0300

    Add Phone annotations

--- src/main/java/com/cecifz/sistemabancario_poo/model/Phone.java
+++ src/main/java/com/cecifz/sistemabancario_poo/model/Phone.java
@@ -1,9 +1,20 @@
 package com.cecifz.sistemabancario_poo.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Phone {
+    @Id
     @Column(length = 20, nullable = false)
+    @EqualsAndHashCode.Include
     private String phoneNumber;
 
     @ManyToOne
